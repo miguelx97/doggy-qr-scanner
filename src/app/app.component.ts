41b2d1f61a3cdc3dd128b7434f1efb9fc5a91395
Miguel Martin <miguel.martin@siptize.com>
2022-08-30 15:49:06 +0200

    added multi language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Device } from '@capacitor/device';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private translate: TranslateService
+  ) {
+    this.translate.setDefaultLang(Language.ENGLISH);
+    this.setLanguage();
+  }
+
+  async setLanguage(){
+    let language:string = Language.ENGLISH;
+    const languageDevice: string = (await Device.getLanguageCode()).value;
+    const listLanguage = Language.list().filter(lang => (new RegExp("\\b" + lang + "\\b")).test(languageDevice))
+    if(listLanguage.length) language = listLanguage[0];
+    if(language !== this.translate.currentLang) this.translate.use(language);
+  }
+}
+
+enum Language {
+  SPANISH = 'es'
+  , ENGLISH = 'en'
+}
+namespace Language {
+  export function list():any[] {
+      return Object.values(Language).slice(0,-1);
+  }
 }
